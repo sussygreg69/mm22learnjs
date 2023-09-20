f46a1c18b9e89535b61206904768f1abff0b74ea
sussygreg69 <gregormalberg16@gmail.com>
2023-09-20 16:03:12 +0300

    style boooo

--- script.js
+++ script.js
@@ -1,66 +1,26 @@
-function hello(name='nimetu', age=Infinity){
-    let yearText = 'years';
-    if(age === 1 ){
-        yearText = 'year'
-    }
-    console.log(`hello ${name} noku ${age} ${yearText}`);
+
+let title = document.getElementById('title');
+console.log({title});
+
+setTimeout(() => {
+    title.innerText = 'fuck you js';
+title.classList.add('green');
+}, 50);
+let r, g, b;
+setInterval(() => {
+r= Math.round(Math.random() * 255);
+g= Math.round(Math.random() * 255);
+b= Math.round(Math.random() * 255);
+document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+if(title.classList.contains('green')) {
+    title.classList.replace('green', 'red');
+} else {
+    title.classList.replace('red', 'green');
 }
+},500);
 
-hello('rögabert', 3);
-hello('vladimir', 1);
-hello();
-
-function volume(h, l, w){
-    return h*w*l;
-}
-
-let answer = volume(4, 2, 5);
-console.log(answer);
-
-function numbers(i) {
-    console.log(i);
-    if(i<10){
-        numbers(i+1)
-    }
-   
-}
-numbers(0);
-
-let nums = [0, 1, 2, 3, 4];
-
-function print(num){
-    console.log(num);
-}
-nums.forEach(print);
-nums.forEach(function (num){
-    console.log('num is ' + num);
+let textinput = document.querySelector('#textinput');
+textinput.addEventListener('input', evt => {
+    console.log(evt);
+    title.innerText = textinput.value.split('').reverse().join('');
 });
-
-nums.forEach(num => {
-    console.log('num is ' + num);
-});
-
-let sums = nums.map(num =>{
-    return num + 10;
-});
-console.log(sums);
-
-sums = nums.map(num => num + 10);
-console.log(sums);
-
-let sum = nums.reduce((total, num) => total + num);
-console.log(sum);
-
-let sorted = nums.sort((a,b)=> {
-    if(a < b) {
-        return 1;
-    } else if(a < b)
-        {return -1;}
-        else {
-            return 0;
-        }
-    });
-    console.log(sorted);
-
-    console.log(Math.PI);
-    console.log(Math.random())
