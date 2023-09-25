f08745e71a27cccf802c2b196ca9c210c0038707
sussygreg69 <gregormalberg16@gmail.com>
2023-09-25 09:08:10 +0300

    dfgfghfghfhg

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-
-let title = document.getElementById('title');
-console.log({title});
-
-setTimeout(() => {
-    title.innerText = 'fuck you js';
-title.classList.add('green');
-}, 50);
-let r, g, b;
-setInterval(() => {
-r= Math.round(Math.random() * 255);
-g= Math.round(Math.random() * 255);
-b= Math.round(Math.random() * 255);
-document.body.style.background = `rgb(${r}, ${g}, ${b})`;
-if(title.classList.contains('green')) {
-    title.classList.replace('green', 'red');
-} else {
-    title.classList.replace('red', 'green');
-}
-},500);
-
-let textinput = document.querySelector('#textinput');
-textinput.addEventListener('input', evt => {
-    console.log(evt);
-    title.innerText = textinput.value.split('').reverse().join('');
-});
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+ctx.strokeStyle = 'red';
+ctx.stroke();
+ctx.font = '30px Arial';
+ctx.fillText('fuck you canvas', 30, 50);
+ctx.strokeText('fuck you canvas', 30, 100);
+ctx.fillStyle = '#ff0';
+ctx.fillRect(10, 10, 10, 10);
