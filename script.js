2bda82117eac68b3b22bbc3889e1c15a055365a6
sussygreg69 <gregormalberg16@gmail.com>
2023-09-25 10:26:43 +0300

    eesti lipp, pentagram ja loll pie chart

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, 150, 53);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 53, 150, 53);
+
+ctx.strokeStyle = 'black';
+ctx.strokeRect(0, 106, 150, 53);
+
+
+ctx.strokeStyle = 'black';
+ctx.beginPath;
+ctx.moveTo(225, 10);
+ctx.lineTo(250, 60); ctx.lineTo(200, 30); ctx.lineTo(255, 30); ctx.lineTo(215, 60);
+ctx.lineTo(225, 10);
+ctx.closePath();
 ctx.stroke();
+
 ctx.beginPath();
-ctx.arc(100, 100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'red';
+ctx.strokeStyle = 'black';
+ctx.arc(225, 125, 30, 0, Math.PI/100*360);
+ctx.closePath();
 ctx.stroke();
-ctx.font = '30px Arial';
-ctx.fillText('fuck you canvas', 30, 50);
-ctx.strokeText('fuck you canvas', 30, 100);
-ctx.fillStyle = '#ff0';
-ctx.fillRect(10, 10, 10, 10);
+
+ctx.beginPath();
+ctx.moveTo(225, 125);
+ctx.lineTo(210, 90); ctx.lineTo(240, 90);
+ctx.lineTo(225, 125);
+ctx.fill();
+
+ctx.beginPath();
+ctx.lineWidth = 19;
+ctx.strokeStyle = 'white';
+ctx.arc(225, 125, 40, 0, Math.PI/100*360);
+ctx.closePath();
+ctx.stroke();
+
+
+
+
+
+
+
+
+
+
